217d8cf08f6693d1902c9d27a093d26d25ca4839
Kate Kashko <katekashkobiz@gmail.com>
2023-03-19 20:24:52 +0300

    Trying to make Traffic light work

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -8,23 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var greenLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var redLight: UIView!
     @IBOutlet var startButton: UIButton!
+    var greenLightOn = true
+    var yellowLightOn = true
+    var redLightOn = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        greenLight.layer.cornerRadius = 50
-        yellowLight.layer.cornerRadius = 50
-        redLight.layer.cornerRadius = 50
+        greenLight.layer.cornerRadius = 55
+        yellowLight.layer.cornerRadius = 55
+        redLight.layer.cornerRadius = 55
         startButton.layer.cornerRadius = 10
-        
     }
-
+    
+    fileprivate func updateUI() {
+        if greenLightOn {
+            greenLight.alpha = 1
+            yellowLight.alpha = 0.3
+            redLight.alpha = 0.3
+        } else {
+            greenLight.alpha = 0.3
+        }
+//        if yellowLightOn {
+//            greenLight.alpha = 0.3
+//            yellowLight.alpha = 1
+//            redLight.alpha = 0.3
+//        } else {
+//            yellowLight.alpha = 0.3
+//        }
+//        if redLightOn {
+//            greenLight.alpha = 0.3
+//            yellowLight.alpha = 0.3
+//            redLight.alpha = 1
+//        } else {
+//            redLight.alpha = 0.3
+//        }
+    }
+    
     @IBAction func startButton(_ sender: UIButton) {
         startButton.setTitle("NEXT", for: .normal)
+            greenLightOn.toggle()
+//            yellowLightOn.toggle()
+//            redLightOn.toggle()
+            updateUI()
     }
+    
+    
 }
-
